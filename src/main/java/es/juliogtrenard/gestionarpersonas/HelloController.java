9c4a6e1660478783850d4d15fae07895d2c878ea
Julio <juliogtrenard@gmail.com>
2024-10-10 14:27:10 +0200

    ADD codigo de la interfaz

--- src/main/java/es/juliogtrenard/gestionarpersonas/HelloController.java
+++ src/main/java/es/juliogtrenard/gestionarpersonas/HelloController.java
@@ -1,14 +1,41 @@
 package es.juliogtrenard.gestionarpersonas;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btnAgregar;
+
+    @FXML
+    private TableColumn<?, ?> tcApellidos;
+
+    @FXML
+    private TableColumn<?, ?> tcEdad;
+
+    @FXML
+    private TableColumn<?, ?> tcNombre;
+
+    @FXML
+    private TableView<?> tvTabla;
+
+    @FXML
+    private TextField txtApellidos;
+
+    @FXML
+    private TextField txtEdad;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    void agregarPersona(ActionEvent event) {
+
     }
+
 }
