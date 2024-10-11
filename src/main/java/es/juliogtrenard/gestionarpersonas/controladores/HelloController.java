012eeefa70474efc156ee2c7dbe5eae51aae1be3
Julio González <juliogtrenard@gmail.com>
2024-10-11 23:54:01 +0200

    ADD método que limpie los campos después de introducir una persona

--- src/main/java/es/juliogtrenard/gestionarpersonas/controladores/HelloController.java
+++ src/main/java/es/juliogtrenard/gestionarpersonas/controladores/HelloController.java
@@ -1,5 +1,6 @@
 package es.juliogtrenard.gestionarpersonas.controladores;
 
+import es.juliogtrenard.gestionarpersonas.modelos.Persona;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,22 +8,24 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+
 public class HelloController {
 
     @FXML
     private Button btnAgregar;
 
     @FXML
-    private TableColumn<?, ?> tcApellidos;
+    private TableColumn<Persona, String> tcApellidos;
 
     @FXML
-    private TableColumn<?, ?> tcEdad;
+    private TableColumn<Persona, Integer> tcEdad;
 
     @FXML
-    private TableColumn<?, ?> tcNombre;
+    private TableColumn<Persona, String> tcNombre;
 
     @FXML
-    private TableView<?> tvTabla;
+    private TableView<Persona> tvTabla;
 
     @FXML
     private TextField txtApellidos;
@@ -34,8 +37,18 @@ public class HelloController {
     private TextField txtNombre;
 
     @FXML
+    private ArrayList<Persona> listaPersonas = new ArrayList<>();
+
+    @FXML
     void agregarPersona(ActionEvent event) {
 
+        limpiarCampos();
+    }
+
+    private void limpiarCampos() {
+        txtApellidos.clear();
+        txtEdad.clear();
+        txtNombre.clear();
     }
 
 }
