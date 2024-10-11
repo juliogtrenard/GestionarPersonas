3da92eebc5c8b18ff52eff7caec6fe8dda2a5ab9
Julio González <juliogtrenard@gmail.com>
2024-10-11 22:53:39 +0200

    Interfaz de usuario terminada

--- src/main/java/es/juliogtrenard/gestionarpersonas/HelloApplication.java
+++ src/main/java/es/juliogtrenard/gestionarpersonas/HelloApplication.java
@@ -11,9 +11,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        stage.setTitle("Gestionar personas");
         stage.setScene(scene);
+
+        stage.setMinWidth(700);
+        stage.setMinHeight(400);
+
         stage.show();
     }
 
