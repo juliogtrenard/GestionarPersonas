8e64f4f9fab49b0dd0252a97bd4128493e47d8dd
Julio González <juliogtrenard@gmail.com>
2024-10-12 21:00:45 +0200

    FIX salen los datos en la tabla cuando se agrega la persona

--- src/main/java/es/juliogtrenard/gestionarpersonas/controladores/HelloController.java
+++ src/main/java/es/juliogtrenard/gestionarpersonas/controladores/HelloController.java
@@ -3,14 +3,12 @@ package es.juliogtrenard.gestionarpersonas.controladores;
 import es.juliogtrenard.gestionarpersonas.modelos.Persona;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.util.ArrayList;
 
 public class HelloController {
+    int contador = 0;
 
     @FXML
     private Button btnAgregar;
@@ -37,12 +35,58 @@ public class HelloController {
     private TextField txtNombre;
 
     @FXML
-    private ArrayList<Persona> listaPersonas = new ArrayList<>();
+    private ArrayList<Persona> listaPersonas;
+
+    @FXML
+    public void initialize() {
+        listaPersonas = new ArrayList<>();
+    }
 
     @FXML
     void agregarPersona(ActionEvent event) {
+        String errores = errores();
 
+        if(errores.isEmpty()) {
+            String nombre = txtNombre.getText();
+            String apellidos = txtApellidos.getText();
+            int edad = Integer.parseInt(txtEdad.getText());
+
+            Persona persona = new Persona(nombre, apellidos, edad);
+            listaPersonas.add(persona);
+
+            tvTabla.getItems().add(persona);
+        } else {
+            Alert alerta = new Alert(Alert.AlertType.ERROR, errores);
+            alerta.setHeaderText(null);
+            alerta.setTitle("Errores en la encuesta:");
+            alerta.showAndWait();
+        }
+
+        contador++;
         limpiarCampos();
+    }
+
+    private String errores() {
+        String errores = "";
+
+        String nombre = txtNombre.getText();
+        String apellidos = txtApellidos.getText();
+
+        if(nombre.isEmpty()) {
+            errores += "Debes introducir tu nombre.\n";
+        }
+
+        if(apellidos.isEmpty()) {
+            errores += "Debes introducir al menos un apellido.\n";
+        }
+
+        try {
+            int edad = Integer.parseInt(txtEdad.getText());
+        } catch (NumberFormatException e) {
+            errores += "El campo 'Edad' debe ser numérico.\n";
+        }
+
+        return errores;
     }
 
     private void limpiarCampos() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,5 +6,7 @@ module es.juliogtrenard.gestionarpersonas {
     opens es.juliogtrenard.gestionarpersonas to javafx.fxml;
     exports es.juliogtrenard.gestionarpersonas;
     exports es.juliogtrenard.gestionarpersonas.controladores;
+    exports es.juliogtrenard.gestionarpersonas.modelos;
     opens es.juliogtrenard.gestionarpersonas.controladores to javafx.fxml;
+    opens es.juliogtrenard.gestionarpersonas.modelos to javafx.fxml;
 }
