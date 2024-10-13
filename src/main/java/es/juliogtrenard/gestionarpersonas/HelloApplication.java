fbd477df1c20386735bc5fcc231efc3bb8194cda
Julio González <juliogtrenard@gmail.com>
2024-10-13 20:50:20 +0200

    ADD icono de la aplicación

--- src/main/java/es/juliogtrenard/gestionarpersonas/HelloApplication.java
+++ src/main/java/es/juliogtrenard/gestionarpersonas/HelloApplication.java
@@ -3,6 +3,7 @@ package es.juliogtrenard.gestionarpersonas;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
-        stage.setTitle("Gestionar personas");
+        stage.getIcons().add(new Image(String.valueOf(getClass().getResource("/img/library_icon.png"))));
+        stage.setTitle("PERSONAS");
         stage.setScene(scene);
 
         stage.setMinWidth(700);
